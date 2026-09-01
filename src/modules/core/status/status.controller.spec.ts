c9e80ea1e64b02bc6ef7a7b2fd03807ef4564a4d
Felipe <felipesouzasalles@gmail.com>
2026-09-01 17:41:39 -0300

    makes `npm test` more robust with `async-retry` and `orchestrator.ts`

--- src/modules/core/status/status.controller.spec.ts
+++ src/modules/core/status/status.controller.spec.ts
@@ -1,10 +1,15 @@
 import { Test, TestingModule } from "@nestjs/testing"
 import { DatabaseModule } from "src/infra/database/database.module"
+import orchestrator from "test/orchestrator"
 import { StatusController } from "./status.controller"
 import { StatusService } from "./status.service"
 
 describe('StatusController', () => {
   let statusController: StatusController
+
+  beforeAll(async () => {
+    await orchestrator.waitForAllServices();
+  })
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
